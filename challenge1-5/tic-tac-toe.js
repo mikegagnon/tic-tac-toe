e39776d32185427f05367b128ed125592e4e7462
Michael Gagnon <mikegagnon@gmail.com>
2017-01-06 13:37:12 -0700

    init challenge 5

---
+++ challenge1-5/tic-tac-toe.js
@@ -0,0 +1,196 @@
+function assert(condition) {
+    if (!condition) {
+        console.error("Assertion failed");
+    }
+}
+
+NUM_ROWS = 3;
+NUM_COLS = 3;
+
+EMPTY = 0;
+PLAYER_X = 1;
+PLAYER_O = 2;
+
+/*******************************************************************************
+ * Move is the interface between TicTacToe and Viz
+ ******************************************************************************/
+class Move {
+    // valid == true iff the move results in change in game state
+    // (row, col) are the coordinates that player added their mark
+    // player is either PLAYER_X or PLAYER_O, depending on who made the move
+    constructor(valid, row, col, player) {
+        this.valid = valid;
+        this.row = row;
+        this.col = col;
+        this.player = player;
+    }
+}
+
+/*******************************************************************************
+ * TicTacToe class
+ ******************************************************************************/
+class TicTacToe {
+
+    // player is either PLAYER_X or PLAYER_O, and indicates which player has
+    // the opening move
+    constructor(player) {
+        this.matrix = [
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]
+        ];
+
+        assert(player == PLAYER_X || player == PLAYER_O);
+
+        // this.player always equals the player (either PLAYER_X or PLAYER_O)
+        // who has the next move.
+        this.player = player;
+    }
+
+    makeMove(row, col) {
+
+        assert(row >= 0 && row < NUM_ROWS);
+        assert(col >= 0 && col < NUM_COLS);
+
+        if (this.matrix[row][col] != EMPTY) {
+            return new Move(false, undefined, undefined, undefined);
+        } 
+
+        this.matrix[row][col] = this.player;
+
+        var move = new Move(true, row, col, this.player);
+
+        if (this.player == PLAYER_X) {
+            this.player = PLAYER_O;
+        } else {
+            this.player = PLAYER_X;
+        }
+
+        return move;
+    }
+
+}
+
+/*******************************************************************************
+ * Vizualization code
+ ******************************************************************************/
+ class Viz {
+    
+    static getCellId(row, col) {
+        return "cell-" + row + "-" + col;
+    }
+
+    getImgTag(player) {
+        var filename;
+        if (player == PLAYER_X) {
+            filename = "player-x.png";
+        } else if (player == PLAYER_O) {
+            filename = "player-o.png";
+        } else {
+            assert(false);
+        }
+
+        return "<img src='" + filename + "' width=" + this.cell_size + " >";
+    }
+
+    constructor(cell_size) {
+        this.cell_size = cell_size;
+
+        $(".cell").css("height", this.cell_size);
+        $(".cell").css("width", this.cell_size);
+    }
+
+    drawMove(move) {
+        if (!move.valid) {
+            return;
+        }
+
+        var cellId = Viz.getCellId(move.row, move.col);
+        var imgTag = this.getImgTag(move.player);
+        $("#" + cellId).append(imgTag);
+    }
+ }
+
+/*******************************************************************************
+ * Controller code
+ ******************************************************************************/
+var GAME = new TicTacToe(PLAYER_X);
+var VIZ = new Viz(100);
+
+function cellClick(row, col) {
+
+    var move = GAME.makeMove(row, col);
+    VIZ.drawMove(move);
+
+}
+
+/*******************************************************************************
+ * TESTS
+ ******************************************************************************/
+
+// Returns true iff the matrices are equal 
+function matricesEqual(matrix1, matrix2) {
+    for (row = 0; row < NUM_ROWS; row++) {
+        for (col = 0; col < NUM_COLS; col++) {
+            if (matrix1[row][col] != matrix2[row][col]) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+// Test player-x makeMove(0, 0)
+var game = new TicTacToe(PLAYER_X);
+game.makeMove(0, 0);
+var expected_matrix = [
+    [PLAYER_X, EMPTY, EMPTY],
+    [EMPTY,    EMPTY, EMPTY],
+    [EMPTY,    EMPTY, EMPTY]
+]
+assert(matricesEqual(game.matrix, expected_matrix));
+
+// Test player-x makeMove(1, 1)
+var game = new TicTacToe(PLAYER_X);
+game.makeMove(1, 1);
+var expected_matrix = [
+    [EMPTY,    EMPTY,    EMPTY],
+    [EMPTY,    PLAYER_X, EMPTY],
+    [EMPTY,    EMPTY,    EMPTY]
+]
+assert(matricesEqual(game.matrix, expected_matrix));
+
+// Test opening player as PLAYER_O
+var game = new TicTacToe(PLAYER_O);
+game.makeMove(0, 0);
+var expected_matrix = [
+    [PLAYER_O, EMPTY,    EMPTY],
+    [EMPTY,    EMPTY,    EMPTY],
+    [EMPTY,    EMPTY,    EMPTY]
+]
+assert(matricesEqual(game.matrix, expected_matrix));
+
+// Test X then O then X
+var game = new TicTacToe(PLAYER_X);
+game.makeMove(1, 1);
+game.makeMove(0, 0);
+game.makeMove(2, 2);
+var expected_matrix = [
+    [PLAYER_O, EMPTY,    EMPTY],
+    [EMPTY,    PLAYER_X, EMPTY],
+    [EMPTY,    EMPTY,    PLAYER_X]
+]
+assert(matricesEqual(game.matrix, expected_matrix));
+
+// Test invalid move
+var game = new TicTacToe(PLAYER_X);
+game.makeMove(0, 0);
+var move = game.makeMove(0, 0);
+assert(!move.valid);
+var expected_matrix = [
+    [PLAYER_X, EMPTY, EMPTY],
+    [EMPTY,    EMPTY, EMPTY],
+    [EMPTY,    EMPTY, EMPTY]
+]
+assert(matricesEqual(game.matrix, expected_matrix));
